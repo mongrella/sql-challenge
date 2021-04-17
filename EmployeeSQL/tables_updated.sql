3005f1a2fbf5a442c56ce3df46f46dd47e5cc900
lucille bluth <melissa.mongrella@northwestern.edu>
2021-04-16 20:34:47 -0500

    updated sales

--- EmployeeSQL/tables_updated.sql
+++ EmployeeSQL/tables_updated.sql
@@ -126,3 +126,18 @@ SELECT last_name, first_name, gender
 FROM employees
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
+
+
+-- List all employees in the Sales department, 
+--including their employee number, last name, first name, and department name
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+JOIN dept_emp ON
+employees.emp_no=dept_emp.emp_no
+INNER JOIN departments ON
+dept_emp.dept_no=departments.dept_no
+WHERE dept_name='Sales';
+
+-- List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
