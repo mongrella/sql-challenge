5ad85fef9f1ba9d6d883e2eaa9fb1e8002a58fe4
lucille bluth <melissa.mongrella@northwestern.edu>
2021-04-17 09:25:31 -0500

    employee last name data

--- EmployeeSQL/tables_updated.sql
+++ EmployeeSQL/tables_updated.sql
@@ -149,3 +149,10 @@ INNER JOIN departments ON
 dept_emp.dept_no=departments.dept_no
 WHERE dept_name='Sales' OR
 dept_name='Development';
+
+--In descending order, list the frequency count of employee last names, 
+--i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(last_name) FROM employees
+GROUP BY last_name
+ORDER BY count(last_name) DESC;
